add63732366fb5c2b7ad319478aff34f4dc16e87
Juan R. Ovalle <Juanrovalle@gmail.com>
2024-10-31 15:55:04 -0400

    Feature - Add search app support

---
+++ src/app/pages/search/search-dialog/search-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-dialog.component.html',
+  styleUrl: './search-dialog.component.css'
+})
+export class SearchDialogComponent {
+
+}
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -5,20 +5,41 @@ import { MatTabGroup } from '@angular/material/tabs';
 import { format } from 'date-fns';
 import { FilterConsultDTO } from '../../model/filterConsultDTO';
 import { ConsultService } from '../../services/consult.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Consult } from '../../model/consult';
+import { DatePipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
+import { SearchDialogComponent } from './search-dialog/search-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [MaterialModule, ReactiveFormsModule],
+  imports: [
+    MaterialModule,
+    ReactiveFormsModule,
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe,
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
+  message: string;
   @ViewChild('tabGroup') tabGroup: MatTabGroup;
-  form: FormGroup;
-dataSource: CdkTableDataSourceInput<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private consultService: ConsultService) {}
+  form: FormGroup;
+  dataSource: MatTableDataSource<Consult>;
+  displayedColumns = ['patient', 'medic', 'date', 'actions'];
+
+  constructor(
+    private consultService: ConsultService,
+    private _dialog: MatDialog
+  ) {}
   ngOnInit(): void {
     this.form = new FormGroup({
       dni: new FormControl(),
@@ -37,18 +58,34 @@ dataSource: CdkTableDataSourceInput<any>;
         dni,
         fullName
       );
-      this.consultService
-        .searchOthers(filterConsultDTO)
-        .subscribe((data) => console.log(data));
+      this.consultService.searchOthers(filterConsultDTO).subscribe((data) => {
+        console.log(data);
+        this.createTable(data);
+      });
     } else {
       // option2
-      const date1 = format(this.form.value['startDate'], "yyyy-MM-dd'T'HH:mm:ss");
+      const date1 = format(
+        this.form.value['startDate'],
+        "yyyy-MM-dd'T'HH:mm:ss"
+      );
       const date2 = format(this.form.value['endDate'], "yyyy-MM-dd'T'HH:mm:ss");
       //service
 
-      this.consultService
-        .searchByDates(date1, date2)
-        .subscribe();
+      this.consultService.searchByDates(date1, date2).subscribe((data) => {
+        this.createTable(data);
+      });
     }
+  }
+  createTable(data: Consult[]) {
+    if (data.length == 0) {
+      this.message = 'No data found';
+    } else {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+  viewDetails(consult: Consult) {
+    this._dialog.open(SearchDialogComponent, { width: '750px', data: consult });
   }
 }
