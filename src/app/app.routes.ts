931fe6837a0dec0550528544e51a4c43f12d3a0b
Juan R. Ovalle <Juanrovalle@gmail.com>
2024-12-10 16:37:48 -0400

    add 404 route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,22 @@ import { Routes } from '@angular/router';
 import { PatientComponent } from './pages/patient/patient.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
+import { Not404Component } from './pages/not404/not404.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { 
+  {
     path: 'pages',
     component: LayoutComponent,
-    loadChildren: () => import('./pages/pages.routes').then(x => x.pageRoutes)
+    loadChildren: () =>
+      import('./pages/pages.routes').then((x) => x.pageRoutes),
   },
-//   {
-//     path: 'admin',
-//     component: LayoutComponent,
-//     loadChildren: () => import('./admin/pages.routes').then(x => x.pageRoutes)
-// },
+  //   {
+  //     path: 'admin',
+  //     component: LayoutComponent,
+  //     loadChildren: () => import('./admin/pages.routes').then(x => x.pageRoutes)
+  // },
+
+  { path: '**', component: Not404Component }
 ];
